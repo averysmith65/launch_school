25b823da0dee2949e998e95d802bad458818802d
Avery Smith <avery@ngparts.com>
2020-07-06 15:13:43 -0500

    added method_exercises

---
+++ method_exercises/ex_4.rb
@@ -0,0 +1,2 @@
+# the code will print nothing to the screen because inside the method
+# the return has nothing after it on the line so it has nothing to return
---
+++ method_exercises/ex_6.rb
@@ -0,0 +1,2 @@
+# the error messege in number 6 tells me that there are a wrong amount of arguments
+# in the method it is looking for 2 but only giving 1
