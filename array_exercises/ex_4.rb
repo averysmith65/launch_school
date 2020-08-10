741eef3358d8a0a3d71e08a33c539591455dd891
Avery Smith <avery@ngparts.com>
2020-08-10 11:52:34 -0500

    added array_exercises

---
+++ array_exercises/ex_4.rb
@@ -0,0 +1,7 @@
+arr = [15, 7, 18, 5, 12, 8, 5, 1]
+#1 will retrun the first index that equals the element arr.index(5) 
+# in this case the index is 3
+
+#2 returns an error because syntax is wrong
+
+#3 arr[5] returns the fifth index which is 8 
---
+++ array_exercises/ex_6.rb
@@ -0,0 +1,2 @@
+# you cant change strings by using a string as a key. 
+# you must you indexes as integers such as names[3] = 'jody'
